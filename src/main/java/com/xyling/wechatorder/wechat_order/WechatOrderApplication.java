7816fb639ca141205df0430ebc410dc447962e0a
徐亚玲 <xyling1024@foxmail.com>
2018-02-28 14:43:57 +0800

    Springboot 项目初始构建完成
    Signed-off-by: 徐亚玲 <xyling1024@foxmail.com>

---
+++ src/main/java/com/xyling/wechatorder/wechat_order/WechatOrderApplication.java
@@ -0,0 +1,16 @@
+package com.xyling.wechatorder.wechat_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatOrderApplication.class, args);
+
+
+
+
+	}
+}
---
+++ src/test/java/com/xyling/wechatorder/wechat_order/WechatOrderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xyling.wechatorder.wechat_order;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WechatOrderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
